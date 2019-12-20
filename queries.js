22a88406d9b23926522cd63f736f31e7080167bd
Caio Viotti <caio@viotti.me>
2019-12-20 18:10:28 -0300

    Add function to retrieve all users from the API

--- queries.js
+++ queries.js
@@ -6,3 +6,13 @@ const pool = new Pool({
   password: '123',
   port: 5432,
 })
+
+// Retrieve all users
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
