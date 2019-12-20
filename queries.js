44e9b97d6d04784e6d2cd2cfb8aa0aaa4c664a85
Caio Viotti <caio@viotti.me>
2019-12-20 19:32:18 -0300

    Add function to update existing users through the API

--- queries.js
+++ queries.js
@@ -40,3 +40,20 @@ const createUser = (request, response) => {
     response.status(201).send(`User added with ID: ${result.insertId}`)
   })
 }
+
+// Edit existing user
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { name, email } = request.body
+
+  pool.query(
+    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+}
