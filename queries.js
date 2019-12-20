12568c4cbebde419e300fea366ed43cb5d5320aa
Caio Viotti <caio@viotti.me>
2019-12-20 18:12:12 -0300

    Add function to retrieve a single user by id

--- queries.js
+++ queries.js
@@ -16,3 +16,15 @@ const getUsers = (request, response) => {
     response.status(200).json(results.rows)
   })
 }
+
+// Retrieve a single user by id
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
