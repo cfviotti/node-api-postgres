bd4c2b5421fdd55466c3e2b78db7415ce53ce958
Caio Viotti <caio@viotti.me>
2019-12-23 10:48:33 -0300

    Setup API endpoints

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const db = require('./queries')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
@@ -17,3 +18,9 @@ app.get('/', (request, response) => {
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
 })
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
